7f2a5a2d9bff5c6cbaa569bfcf648ce60f032f4d
Oleg Yablokov <keltekpf@gmail.com>
2020-06-26 02:38:48 +0300

    Collision of world borders with buildings
    Collision of world borders with buildings during their generation is determined.

--- EntityPlacer.py
+++ EntityPlacer.py
@@ -4,8 +4,12 @@ from Buildings import Anthill
 
 def init_entity(building, ex_map):
     random.seed(ex_map.seed)
-    rand_x = int((random.randint(0, ex_map.width)) / ex_map.scale)
-    rand_y = int((random.randint(0, ex_map.height)) / ex_map.scale)
+    rand_x = int(random.randint(0, ex_map.width/ex_map.scale))
+    if rand_x > int(ex_map.width/ex_map.scale) - building.size:
+        rand_x -= building.size
+    rand_y = int(random.randint(0, ex_map.height/ex_map.scale))
+    if rand_y > int(ex_map.height/ex_map.scale) - building.size:
+        rand_y -= building.size
     for x in range(building.size):
         for y in range(building.size):
             ex_map.TileArray[rand_x + x][rand_y + y].image.fill(building.color)
