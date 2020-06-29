79d1e1462da34f120612cd78760aa4dbf75797a4
Oleg Yablokov <keltekpf@gmail.com>
2020-06-30 01:51:13 +0300

    Watch function for Ant

--- Ant.py
+++ Ant.py
@@ -9,9 +9,20 @@ class Ant(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.color = (0, 0, 0)
         self.image.fill(self.color)
+        self.rect.center = 250, 250
         self.name = "ant"
         self.id = 30
-        self.rect.center = 250, 250
+        self.vision = 20
 
     def update(self):
         pass
+
+    def watch(self):
+        side_ax = int(self.rect.x / self.scale - self.vision)
+        side_bx = int(self.rect.x / self.scale + self.vision)
+        side_ay = int(self.rect.y / self.scale - self.vision)
+        side_by = int(self.rect.y / self.scale + self.vision)
+
+        for x in range(side_ax, side_bx):
+            for y in range(side_ay, side_by):
+                pass
