1349882e7cfcd147d8ed5912d20f3fa8c11f606a
Oleg Yablokov <keltekpf@gmail.com>
2020-06-30 14:47:03 +0300

    Add vision matrix

--- Ant.py
+++ Ant.py
@@ -13,6 +13,7 @@ class Ant(pygame.sprite.Sprite):
         self.name = "ant"
         self.id = 30
         self.vision = 20
+        self.vision_matrix = (0, 0, 0, 0)
         self.target = False
         self.target_position = (0, 0)
 
@@ -27,6 +28,15 @@ class Ant(pygame.sprite.Sprite):
         side_bx = int(self.rect.x / self.scale + self.vision)
         side_ay = int(self.rect.y / self.scale - self.vision)
         side_by = int(self.rect.y / self.scale + self.vision)
+        if side_ax < 0:
+            side_ax = 0
+        if side_bx >= ex_map.width / ex_map.scale:
+            side_bx = int(ex_map.width / ex_map.scale) - 1
+        if side_ay < 0:
+            side_ay = 0
+        if side_by >= ex_map.height / ex_map.scale:
+            side_by = int(ex_map.height / ex_map.scale) - 1
+        self.vision_matrix = (side_ax, side_bx, side_ay, side_by)
 
         for x in range(side_ax, side_bx):
             for y in range(side_ay, side_by):
@@ -36,3 +46,7 @@ class Ant(pygame.sprite.Sprite):
 
     def move_to_cell(self, x, y):
         self.rect.center = (x * self.scale, y * self.scale)
+
+    def get_cell(self):
+        return int(self.rect.x / self.scale), int(self.rect.y / self.scale)
+
