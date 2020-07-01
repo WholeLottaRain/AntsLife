7d7974833bc6e4120d474505d6cee532a2c04032
Oleg Yablokov <keltekpf@gmail.com>
2020-07-02 00:22:47 +0300

    Faster search for the path to the nearest berry

--- Ant.py
+++ Ant.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from PathFinder import find_path
 
 
@@ -40,17 +41,19 @@ class Ant(pygame.sprite.Sprite):
         if side_by >= ex_map.height / ex_map.scale:
             side_by = int(ex_map.height / ex_map.scale) - 1
         self.vision_matrix = (side_ax, side_ay, side_bx, side_by)
-
+        path_length = 999
         for x in range(side_ax, side_bx + 1):
             for y in range(side_ay, side_by + 1):
                 if ex_map.TileArray[x][y].id == 3 and ex_map.TileArray[x][y].eaten is False \
                         and ex_map.TileArray[x][y].booked is False:
-                    self.target = True
-                    self.target_position = x, y
-                    found_path = find_path(ex_map, self.vision_matrix, self.get_cell(), self.target_position)
-                    if (len(self.path) != 0 and len(found_path) < len(self.path)) or len(self.path) == 0:
-                        self.path = found_path
+                    hypotenuse = math.sqrt((self.get_cell()[0]-x)*(self.get_cell()[0]-x) + (self.get_cell()[1]-y)*(self.get_cell()[1]-y))
+                    if hypotenuse < path_length:
+                        path_length = hypotenuse
+                        self.target = True
+                        self.target_position = x, y
+
         if self.target is True:
+            self.path = find_path(ex_map, self.vision_matrix, self.get_cell(), self.target_position)
             self.target_position = self.path[len(self.path) - 1][0], self.path[len(self.path) - 1][1]
             ex_map.TileArray[self.target_position[0]][self.target_position[1]].booked = True
             print("Going to: " + str(self.path[len(self.path) - 1][0]) + " " + str(
